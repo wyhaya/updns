b32f1823eb4226f8c7e0bc0d09692164cb828b16
wyhaya <wyhaya@gmail.com>
2018-06-22 11:49:33 +0800

    update log format

--- bin/log.js
+++ bin/log.js
@@ -5,7 +5,7 @@ const path = require('path')
 const logConfigPath = path.join(__dirname, './../log/updns.log')
 
 
-function write(log) {
+const echo = log => {
 
     fs.appendFile(logConfigPath, log, {
         encoding: 'utf8'
@@ -20,9 +20,25 @@ function write(log) {
 }
 
 
-module.exports = (domain) => {
+const getDateTime = () => {
 
-    const log = `${new Date().toString()}    ${domain}\n`
+    const date = new Date()
+    let y = date.getFullYear().toString()
+    let m = (date.getMonth() + 1).toString().padStart(2, '0')
+    let d = date.getDate().toString().padStart(2, '0')
+    let hr = date.getHours().toString().padStart(2, '0')
+    let mi = date.getMinutes().toString().padStart(2, '0')
+    let se = date.getSeconds().toString().padStart(2, '0')
+    let ms = date.getMilliseconds().toString().padStart(3, '0')
+
+    return `${y}-${m}-${d} ${hr}:${mi}:${se}.${ms}`
+
+}
+
+
+const write = domain => {
+
+    const log = `${getDateTime()}    ${domain}\n`
 
     fs.stat(path.join(__dirname, './../log'), err => {
 
@@ -32,12 +48,16 @@ module.exports = (domain) => {
                 err => {
                     if(err) {
                         return
-                    }else write(log)
+                    }else echo(log)
                 }
             )
-        } else write(log)
+        } else echo(log)
     })
     
 }
 
 
+module.exports = {
+    getDateTime,
+    write
+}
--- test/log.test.js
+++ test/log.test.js
@@ -3,37 +3,46 @@
 const fs = require('fs')
 const test = require('ava')
 const path = require('path')
-const writeLog = require('./../bin/log')
+const log = require('./../bin/log')
 const logPath = path.join(__dirname, './../log/updns.log')
 
 
 test('Write log', async t => {
 
-    writeLog(Date.now())
+    Number.prototype[Symbol.iterator] = function * () {
+        for(let i = 0; i <= this - 1; i++) yield i
+    }
+
+    const count = [...1000]
+    log.write(Date.now())
 
     await new Promise(waiting => setTimeout(() => {
-        for(let i = 0; i < 8; i++){
-            writeLog(Date.now())
-        }
+        count.map(() => log.write(Date.now()))
         waiting()
     }, 10))
 
-    const logs = new Promise((success, fail) => setTimeout(() => {
+    const logs = new Promise(success => setTimeout(() => {
 
-        fs.readFile(logPath, {
+        let content = ''
+        content = fs.readFileSync(logPath, {
             encoding: 'utf8'
-        }, (err, content) => {
-
-            fs.unlinkSync(logPath)
-
-            if (err) fail()
-            else success(content.split('\n').length)
-
         })
+        fs.unlinkSync(logPath)
+        success(content.split('\n').length)
 
     }, 100))
 
-    t.is(await logs, 10)
+    t.is(await logs, count.length + 2)
+
+})
+
+
+test('Get date time', t => {
+
+    const [data, time] = log.getDateTime().split(' ')
+
+    t.is(/^\d{4}-(0[1-9]|1[0-2])-((0|1|2)[0-9]|3[0-1])$/.test(data), true) // ?
+    t.is(/^(\d{2}:){2}\d{2}.\d{3}$/.test(time), true)
 
 })
 
